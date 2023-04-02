38c40cceae40b4439fd2f33f221ef9e27db64f82
Ohsarsour <obaidullahsarsour@gmail.com>
2023-04-02 16:38:35 -0400

    SQLAlchemy DB and Movie model

--- src/models.py
+++ src/models.py
@@ -1,1 +1,12 @@
 # TODO - Create SQLAlchemy DB and Movie model
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class Movie(db.Model):
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    director = db.Column(db.String(255), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<Movie {self.movie_id}: {self.title} ({self.director}), rating {self.rating}>"
